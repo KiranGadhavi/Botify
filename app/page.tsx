558b09ae0f02e020457dab276c6210607767c55a
Kiran Gadhavi <kirangadhavi@hotmail.com>
2024-09-20 15:43:09 +0100

    deploying chat bot

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
-'use client'
+"use client";
 
-import React from 'react';
-import styles from'./page.module.css';
-import Lottie  from 'lottie-react';
-import animationData from './components/animation.json'; 
-import QuestionContainer from './components/questionContainer';
+import React from "react";
+import styles from "./page.module.css";
+import Lottie from "lottie-react";
+import animationData from "./components/animation/animation.json";
+import QuestionContainer from "./components/questionContainer";
 
 function App() {
   //const [question, setQuestion] = useState('');
   //const [answer, setAnswer] = useState('');
 
   //const handleQuestionSubmit = () => {
-    // Add logic to interact with AI here
-    //setAnswer("This is the response to your question.");
+  // Add logic to interact with AI here
+  //setAnswer("This is the response to your question.");
   //};
 
   return (
@@ -20,7 +20,7 @@ function App() {
       <header>
         <h1 className={styles.title}>The Know-it-all</h1>
         <div className={styles.icon}>
-        <Lottie animationData={animationData} loop={true} />
+          <Lottie animationData={animationData} loop={true} />
         </div>
       </header>
       <QuestionContainer />
